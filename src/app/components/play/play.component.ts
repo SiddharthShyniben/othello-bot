1346a2a9e3626bc86647fc8e696ff03544a5c06d
SiddharthShyniben <siddharth.muscat@gmail.com>
2021-04-25 13:09:34 +0530

    created gh-pages branch

---
+++ src/app/components/play/play.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-play',
+  templateUrl: './play.component.html',
+  styleUrls: ['./play.component.sass']
+})
+export class PlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
